abcdd5b06f62e2c8ee8fd8ef1ce12962c1894a52
hezy <he18270675493@163.com>
2024-07-31 15:14:21 +0800

    test：新增分支，修改接口地址

--- src/main/java/com/hezy/controller/DemoController.java
+++ src/main/java/com/hezy/controller/DemoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @GetMapping("/demo")
+    @GetMapping("/hello")
     public String demo() {
         return "hello";
     }
