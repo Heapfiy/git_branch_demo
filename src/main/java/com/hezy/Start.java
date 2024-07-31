e4d2f54c79be6de9c92123eb5516027b51a74bb6
hezy <he18270675493@163.com>
2024-07-31 15:13:19 +0800

    init：起源

---
+++ src/main/java/com/hezy/Start.java
@@ -0,0 +1,15 @@
+package com.hezy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hezhongying
+ * @create 2024/7/31 15:10
+ */
+@SpringBootApplication
+public class Start {
+    public static void main(String[] args) {
+        SpringApplication.run(Start.class, args);
+    }
+}
---
+++ src/main/java/com/hezy/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.hezy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hezhongying
+ * @create 2024/7/31 15:10
+ */
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "hello";
+    }
+}
